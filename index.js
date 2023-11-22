c04dc98875627ee1b426e0941e1d1c54541cb6bf
Okem Onyeka <jesse007onyeka@gmail.com>
2023-11-22 11:23:16 +0100

    few tweaks

--- index.js
+++ index.js
@@ -2,42 +2,50 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(PORT, () => {
+app.listen(port, () => {
     initialisePuppeteer();
-    console.log(`server is running on port: ${PORT}`);
+    console.log(`server is running on port: ${port}`);
 });
 
 const url = 'https://myanimelist.net/anime/54595/Kage_no_Jitsuryokusha_ni_Naritakute_2nd_Season';
 
 const initialisePuppeteer = async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    try{
+        const browser = await puppeteer.launch({ headless: false });
 
-    const page = await browser.newPage();
+        const page = await browser.newPage();
 
-    await page.viewport({
-        width: 1920,
-        height: 1080
-    });
+        await page.viewport({
+            width: 2560,
+            height: 1440
+        });
 
-    await page.goto(url);
+        await page.goto(url);
 
-    await page.waitForSelector('div[class = "tbl-next-up-closeBtn"]');
-    await page.click('div[class = "tbl-next-up-closeBtn"]');
+        await page.waitForSelector('div[class = "tbl-next-up-closeBtn"]');
+        await page.click('div[class = "tbl-next-up-closeBtn"]');
+        await page.waitForTimeout(1000);
 
-    // await page.waitForSelector('button');
-    // await page.click('button');
+        await page.waitForSelector('button');
+        await page.click('button');
 
-    await page.click('a[id = "malLogin"]');
+        await page.click('a[id = "malLogin"]');
 
-    await page.type('input[id = "loginUserName"]', process.env.USERNAME);
-    await page.type('input[id = "password"]', process.env.PASSWORD);
-    await page.click('button[type = "submit"]');
+        await page.type('input[id = "loginUserName"]', process.env.USERNAME);
+        await page.type('input[id = "password"]', process.env.PASSWORD);
+        await page.click('button[type = "submit"]');
 
-    await page.click('a[class = "js-btn-count increase ml4"]');
-}
+        await page.waitForTimeout(3000);
+
+        await page.click('a[class = "js-btn-count increase ml4"]');
+        
+    } catch (err){
+        console.log(error);
+    }
+};
